23e8f24fb1c477a8ee97424119363ce959913435
neonphog <neonphog@gmail.com>
2019-04-11 16:49:06 -0600

    fix unbound variable && fill out architectures

--- step2-node-static-build.bash
+++ step2-node-static-build.bash
@@ -49,15 +49,17 @@ pxz -dc "${docker_img_file}" | docker load
 # -- execute docker script -- #
 
 log "execute docker build"
-if [ "x${CI_RUN_MIN}" == "x" ]; then
+if [ -z ${CI_RUN_MIN+x} ]; then
+  log "running complete script"
   docker run --rm -it -v "$(pwd):/work" -u "$(id -u ${USER}):$(id -g ${USER})" "${docker_img}" /bin/bash /work/node-static-build-script.bash
 else
-  docker run --rm -it -v "$(pwd):/work" -u "$(id -u ${USER}):$(id -g ${USER})" "${docker_img}" /bin/bash -c "timeout ${CI_RUN_MIN}m /bin/bash /work/node-static-build-script.bash" || true
+  log "running script with CI_RUN_MIN: ${CI_RUN_MIN-40}m"
+  docker run --rm -it -v "$(pwd):/work" -u "$(id -u ${USER}):$(id -g ${USER})" "${docker_img}" /bin/bash -c "timeout ${CI_RUN_MIN-40}m /bin/bash /work/node-static-build-script.bash" || true
 fi
 
 # -- release the bits -- #
 
-if [ "x${CI_RUN_MIN}" == "x" ]; then
+if [ -z ${CI_RUN_MIN+x} ]; then
   log "package release"
   cp "${node_src}/build-partly/usr/bin/node" "${node_bin_base}-partly-static"
   pxz "${node_bin_base}-partly-static"
@@ -69,7 +71,7 @@ if [ "x${CI_RUN_MIN}" == "x" ]; then
   tar -I pxz -cf "${node_src}-${build_num}-npm.tar.xz" npm
   release "${node_src}-${build_num}-npm.tar.xz"
 else
-  log "not releasing, CI_RUN_MIN was set: ${CI_RUN_MIN}m"
+  log "not releasing, CI_RUN_MIN was set: ${CI_RUN_MIN-}m"
 fi
 
 # -- done -- #
